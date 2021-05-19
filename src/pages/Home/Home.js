5c6c4d25927fd3fab74c115348fb33d4d1534ae2
Micaela Ramos <micaela.ramos@mercadolibre.com>
2021-05-18 21:59:41 -0300

    Add material ui library and login mocked page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,37 @@
 import React from 'react';
 
-import { Button } from 'antd';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-const Home = () => (
-  <div className="home-container">
-    <h1>HomePage</h1>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
-    <div className="home-content">
-      <Button type="primary">Primary button</Button>
+const Home = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <h1>HomePage</h1>
+
+      <div className="home-content">
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default Home;
