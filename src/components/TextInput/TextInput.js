26bbe33b747f0eac694a575d8e5d7936c15c848a
Micaela Ramos <micaela.ramos@mercadolibre.com>
2021-05-21 17:17:30 -0300

    Delete unused import

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import classNames from 'classnames';
 
 import { colors } from '../../styles/palette';
