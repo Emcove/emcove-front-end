41abd44c1c9ea313555ac53566840d6fbb4495c0
Julian <julyysanchez@hotmail.com>
2021-07-01 21:35:43 -0300

    Change constants.js API_URL to prod

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,3 @@
 // Constants.js
-//export const API_URL= 'https://emcove-back-end.herokuapp.com';
-export const API_URL = 'http://localhost:8081';
+export const API_URL= 'https://emcove-back-end.herokuapp.com';
+//export const API_URL = 'http://localhost:8081';
