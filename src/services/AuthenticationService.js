a90b4784d33b130e7ec206a3f3dbc26000180cb5
Micaela Ramos <micaela.ramos@mercadolibre.com>
2021-08-02 22:53:53 -0300

    Remove setAxiosInterceptor on login

--- src/services/AuthenticationService.js
+++ src/services/AuthenticationService.js
@@ -12,7 +12,7 @@ class AuthenticationService {
                     authorization: basicAuthHeader
                 }
             });
-            this.setupAxiosInterceptor(basicAuthHeader);
+            
             localStorage.setItem('user', JSON.stringify(resp.data));
             return resp;
         }catch(error){
