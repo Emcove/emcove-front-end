c6acc1fa4a1218bde4c3b201c33b632bf9c11c00
Micaela Ramos <micaela.ramos@mercadolibre.com>
2021-10-13 20:20:21 -0300

    add address list and style box

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -5,14 +5,40 @@ import styled from 'styled-components';
 import Modal from '../Modal';
 import TextInput from '../TextInput';
 import Subtitle from '../Subtitle';
+import Button from '../Button';
+import Icon from '../Icons';
+
+import { colors } from '../../styles/palette';
 
 const Container = styled.div`
   width: 100%;
   height: 600px;
 `;
 
-const Location = ({ visible, closeModal }) => {
+const Group = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const AddressItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 12px;
+  border-bottom: solid 1px ${colors.grayBorder};
+`;
+
+const AddressList = styled.div``;
+
+const Text = styled.span`
+  font-size: 16px;
+  line-height: 1.25;
+  color: ${colors.textColor};
+`;
+
+const Location = ({ visible, closeModal, businessLocations, locations }) => {
   const [address, setAddress] = useState('');
+  const [chosenAddress, setChosenAddress] = useState(undefined);
   const [addressList, setAddressList] = useState([]);
 
   const google = window.google;
@@ -29,23 +55,52 @@ const Location = ({ visible, closeModal }) => {
     const address = place.address_components;
 
     if (address) {
+      const objAddress = {};
+      address.reduce((_, curr) => {
+        objAddress[curr.types[0]] = curr.long_name;
+      }, objAddress);
+      
+      setChosenAddress({ ...objAddress, displayName: place.formatted_address });
       setAddress(place.formatted_address);
     }
   };
 
   autocomplete.addListener('place_changed', handleChange);
 
+  const addNewLocation = () => {
+    if (chosenAddress) {
+      setAddressList([...addressList, chosenAddress]);
+      setAddress('');
+      setChosenAddress(undefined);
+    }
+  }
+
   return (
     <Modal open={visible} minWidth="70%" setVisibility={closeModal}>
       <Container>
         <Subtitle>Agregar puntos de entrega</Subtitle>
-        <TextInput
-          id="locationInput"
-          label="Dirección"
-          value={address}
-          type="text"
-          onChange={(value) => setAddress(value)}
-        />
+        <Group>
+          <TextInput
+            id="locationInput"
+            label="Dirección del punto de entrega"
+            value={address}
+            type="text"
+            onChange={(value) => setAddress(value)}
+            placeholder="Ingresá una dirección para el punto de entrega"
+          />
+          <Button backgroundColor={colors.success} onClick={addNewLocation} alignment="center">
+            <Icon type="check" className="done-button__icon"/>
+          </Button>
+        </Group>
+        {!!addressList.length &&
+          <AddressList>
+          {addressList.map(add => (
+            <AddressItem>
+              <Text>{add.displayName}</Text>
+            </AddressItem>
+          ))}
+          </AddressList>
+        }
       </Container>
     </Modal>
   )
