749265aa42daaa5f28c3fa64cd103cc9f07c9620
Micaela Ramos <micaela.ramos@mercadolibre.com>
2021-10-25 08:39:10 -0300

    uncomment prod url

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,6 @@
 // Constants.js
-// export const API_URL= 'https://emcove-back-end.herokuapp.com';
-export const API_URL = 'http://localhost:8081';
+export const API_URL= 'https://emcove-back-end.herokuapp.com';
+// export const API_URL = 'http://localhost:8081';
 
 export const GCLIENT_ID = '487178283114-f8n875bv9alrsicg0lkpr7n4oefq002d.apps.googleusercontent.com';
 export const GAPI_KEY = 'AIzaSyCF2hDR3wSyO7XYHaAHHZJmQWvTQTkUrvM';
